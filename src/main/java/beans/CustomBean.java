92ce46ba1860147b790ad7d9bab1936d00563c8b
bookgvi <bookgvi@gmail.com>
2020-10-05 13:48:40 +0300

    some bug fixes

--- src/main/java/beans/CustomBean.java
+++ src/main/java/beans/CustomBean.java
@@ -5,6 +5,6 @@ import scopes.customScope.CustomAppScope;
 @CustomAppScope
 public class CustomBean implements Greeting {
   public String greeting() {
-    return "Hello, form bean";
+    return "Hello, from bean";
   }
 }
--- src/main/java/scopes/customScope/CustomScopeExtension.java
+++ src/main/java/scopes/customScope/CustomScopeExtension.java
@@ -1,15 +1,16 @@
 package scopes.customScope;
 
+import javax.enterprise.event.Observes;
 import javax.enterprise.inject.spi.AfterBeanDiscovery;
 import javax.enterprise.inject.spi.BeforeBeanDiscovery;
 import javax.enterprise.inject.spi.Extension;
 
 public class CustomScopeExtension implements Extension {
-  public void beforeBeanDiscovery(BeforeBeanDiscovery bbd) {
+  public void beforeBeanDiscovery(@Observes BeforeBeanDiscovery bbd) {
     bbd.addScope(CustomAppScope.class, true, false);
   }
 
-  public void afterBeanDiscovery(AfterBeanDiscovery abd) {
+  public void afterBeanDiscovery(@Observes AfterBeanDiscovery abd) {
     abd.addContext(new CustomScopeContext());
   }
 }
