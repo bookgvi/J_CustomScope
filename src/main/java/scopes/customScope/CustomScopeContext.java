7769078ea1a266410a44d88eb3145459687a352f
bookgvi <bookgvi@gmail.com>
2020-10-07 09:38:46 +0300

    checking value and type of contextual

--- src/main/java/scopes/customScope/CustomScopeContext.java
+++ src/main/java/scopes/customScope/CustomScopeContext.java
@@ -5,9 +5,9 @@ import javax.enterprise.context.spi.Contextual;
 import javax.enterprise.context.spi.CreationalContext;
 import java.lang.annotation.Annotation;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class CustomScopeContext implements Context {
-
   private final ConcurrentHashMap<Contextual<?>, Object> instances = new ConcurrentHashMap<>();
 
   public Class<? extends Annotation> getScope() {
@@ -16,7 +16,11 @@ public class CustomScopeContext implements Context {
 
   public <T> T get(final Contextual<T> contextual, final CreationalContext<T> creationalContext) {
     @SuppressWarnings("unchecked")
-    T instance = (T) instances.computeIfAbsent(contextual, c -> contextual.create(creationalContext));
+    T instance = (T) instances.computeIfAbsent(contextual, c -> {
+      System.out.printf("QQQ %s%n", c.getClass().getName());
+      System.out.printf("QQQ %s%n", contextual.getClass().getName());
+      return contextual.create(creationalContext);
+    });
     return instance;
   }
 
